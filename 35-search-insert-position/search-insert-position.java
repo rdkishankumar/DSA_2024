2efeab67344166735017358b2c877da8159a151d
rdkishankumar <74703599+rdkishankumar@users.noreply.github.com>
2024-09-01 23:18:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (98.69%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -9,13 +9,15 @@ class Solution {
         while(start<=end){
             int mid = start+(end-start)/2;
             if(nums[mid]==target){
-                return mid;
+                result = mid;
+                return result;
             }else if(nums[mid]>target){
                 end = mid-1;
             }else{
+                result = mid;
                 start = mid+1;
             }
         }
-            return start;
+            return result+1;
     }
 }
