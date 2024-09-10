bc0612c7f3b3245b260e1d30225d2d3c3fdb05a0
rdkishankumar <74703599+rdkishankumar@users.noreply.github.com>
2024-09-10 22:38:18 +0530

    Time: 4 ms (99.35%) | Memory: 45.9 MB (5.11%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean validPalindrome(String s) {
         int start = 0;
-        int end = s.length()-1;
-        while(start<end){
-            if(s.charAt(start) == s.charAt(end)){
-                start++;
-                end--;
-            }else{
-                return isPalindrome(s, start+1,end) || isPalindrome(s, start,end-1);
-            }
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end))
+                return isPalindrome(s, start + 1, end) || isPalindrome(s, start, end - 1);
+            start++;
+            end--;
         }
-        return true; 
+        return true;
     }
-    private boolean isPalindrome(String str, int start, int end){
-        while(start<end){
-            if(str.charAt(start) == str.charAt(end)){
-                start++;
-                end--;
-            }else{
+
+    private boolean isPalindrome(String str, int start, int end) {
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
                 return false;
-            }
-            
+            start++;
+            end--;
         }
         return true;
     }
