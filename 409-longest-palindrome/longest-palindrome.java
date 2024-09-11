8917c0035d19ba55d6fb3395e5a7ff532ce8353f
rdkishankumar <74703599+rdkishankumar@users.noreply.github.com>
2024-09-11 21:57:16 +0530

    Time: 7 ms (53.03%) | Memory: 42 MB (22.96%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,6 +1,7 @@
 class Solution {
     public int longestPalindrome(String s) {
         Map<Character, Integer> freq = new HashMap<>();
+        //count frequency of each Character
         for(char ch: s.toCharArray()){
             freq.put(ch, freq.getOrDefault(ch,0)+1);
         }
@@ -8,6 +9,7 @@ class Solution {
         boolean isOddExist = false;
 
         for(int count: freq.values()){
+            //take even count- let's if count = 3 then 3/2 = 1 and 1*2 = 2
             length+= (count/2)*2;
             if(count%2==1){
                 isOddExist =true;
