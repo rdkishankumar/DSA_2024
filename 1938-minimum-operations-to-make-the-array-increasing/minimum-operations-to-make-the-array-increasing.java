82117747e46db23d68b9d76e13af3273fcf4f3a8
rdkishankumar <74703599+rdkishankumar@users.noreply.github.com>
2024-09-13 01:17:01 +0530

    Time: 4 ms (78.36%) | Memory: 44.9 MB (70.15%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -1,8 +1,7 @@
 class Solution {
     public int minOperations(int[] nums) {
         int operations = 0;
-
-        // Find the maximum element in the array
+        
         for (int index = 1; index < nums.length; index++) {
             if (nums[index] <= nums[index - 1]) {
                int increament = nums[index - 1] - nums[index] + 1;
