c888551d4f1539d4f3196af1fd4a0d51c138771a
rdkishankumar <74703599+rdkishankumar@users.noreply.github.com>
2024-09-13 23:44:58 +0530

    Time: 2 ms (100.00%) | Memory: 57.6 MB (55.23%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -6,16 +6,16 @@ class Solution {
         for (int i = 1; i < n; i++) {
             prefix[i] = prefix[i - 1] ^ arr[i];
         }
-        int[] res = new int[queries.length];
+        int[] result = new int[queries.length];
         for (int k = 0; k < queries.length; k++) {
             int i = queries[k][0];
             int j = queries[k][1];
             if (i == 0) {
-                res[k] = prefix[j];
+                result[k] = prefix[j];
             } else {
-                res[k] = prefix[j] ^ prefix[i - 1];
+                result[k] = prefix[j] ^ prefix[i - 1];
             }
         }
-        return res;
+        return result;
     }
 }
